7300210a7fe05ede5e4b1d3fc058345f451cc778
Manolo Salsas <manolez@gmail.com>
2015-12-15 17:10:07 +0100

    QS-900: Add styles for left panel

--- src/js/components/LeftPanel.js
+++ src/js/components/LeftPanel.js
@@ -1,7 +1,6 @@
 import React, { PropTypes, Component } from 'react';
 import { Link } from 'react-router';
 import shouldPureComponentUpdate from 'react-pure-render/function';
-import selectn from 'selectn';
 
 export default class LeftPanel extends Component {
     static propTypes = {
@@ -20,7 +19,7 @@ export default class LeftPanel extends Component {
                     <div className="content-block top-menu">
                         <a className="close-panel" href="#"><span className="icon-notifications"></span><span className="notifications-alert"></span></a>
                     </div>
-                    <div className="content-block">
+                    <div className="content-block profile-block">
                         <div className="profile-image">
                             <img src={`https://dev.nekuno.com/media/cache/resolve/profile_picture/user/images/${user.picture }`} />
                         </div>
@@ -29,19 +28,31 @@ export default class LeftPanel extends Component {
                                 {user.username}
                             </div>
                             <div className="user-location">
-                                <span className="icon-marker"></span> {selectn('location.address', user)}
+                                <span className="icon-marker"></span> {location.address ? location.address : 'Madrid'}
                             </div>
                             <div className="user-interests">
-                                {user.interests}
+                                <div className="number">
+                                    {user.interests ? user.interests : 0}
+                                </div>
+                                <div className="label">
+                                    Intereses
+                                </div>
                             </div>
                         </div>
-                        <div className="content-block">
-                            <div className="menu">
-                                <Link to={`/login2/${user.qnoow_id}`}>
-                                    Hilos
-                                </Link>
-                            </div>
-                        </div>
+                    </div>
+                    <div className="content-block menu">
+                        <Link to={`/login/${user.qnoow_id}`}>
+                            Hilos
+                        </Link>
+                        <Link to={`/login/${user.qnoow_id}`}>
+                            Mi perfil
+                        </Link>
+                        <Link to={`/login/${user.qnoow_id}`}>
+                            Grupos
+                        </Link>
+                        <Link to={`/login/${user.qnoow_id}`}>
+                            Mensajes
+                        </Link>
                     </div>
                 </div>
             </div>

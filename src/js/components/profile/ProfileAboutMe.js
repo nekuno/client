f7616e7ed03a853d642122c15f3c1c43def778f6
yawmoght <yawmoght@gmail.com>
2016-01-13 14:02:22 +0100

    QS-907: Fix some profile labeling issues and add "see more" to About Me.

--- src/js/components/profile/ProfileAboutMe.js
+++ src/js/components/profile/ProfileAboutMe.js
@@ -9,17 +9,49 @@ export default class ProfileAboutMe extends Component {
     };
 
     //shouldComponentUpdate = shouldPureComponentUpdate;
+    constructor(props) {
+        super(props);
+        this.toggleSeeMore = this.toggleSeeMore.bind(this);
+    }
 
     render() {
-        const {name, value} = this.props;
-
+        let {name, value} = this.props;
+        const seeMore = this.state? this.state.seeMore : false;
+        const maxLength = 100;
+        let seeMoreText = "";
+        if (value.length > maxLength){
+            if (seeMore){
+                seeMoreText = 'Ver menos';
+            } else {
+                value = value.substring(0,maxLength)+'...';
+                seeMoreText = 'Ver más';
+            }
+        }
         return (
             <div className="profileAboutMe">
                 <div className="profileAboutMeName">{name}</div>
 
-                <div className="profileAboutMeValue">{value}</div>
+                <div className="profileAboutMeValue">
+                    {value}
+                    {value.length > maxLength
+                        ?
+                        <span className="seeMore" onClick={this.toggleSeeMore}>{' '+seeMoreText}</span>
+                        : ""
+                    }
+
+                </div>
+
             </div>
+
         );
+    }
+
+    toggleSeeMore()
+    {
+        let newState = this.state != null? this.state : {};
+        newState.seeMore = !newState.seeMore;
+        this.setState(newState);
+        //this.state['seeMore'] = (this.state !== null)? !this.state.seeMore : true;
     }
 
 }

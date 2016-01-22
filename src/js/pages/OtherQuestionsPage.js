7990483c01db4d7c007ce7b7c4491e358faf919c
Manolo Salsas <manolez@gmail.com>
2016-01-22 11:30:29 +0100

    QS-905: Add header for other questions page

--- src/js/pages/OtherQuestionsPage.js
+++ src/js/pages/OtherQuestionsPage.js
@@ -7,9 +7,11 @@ import * as UserActionCreators from '../actions/UserActionCreators';
 import RegularTopNavbar from '../components/ui/RegularTopNavbar';
 import ToolBar from '../components/ui/ToolBar';
 import OtherQuestionList from '../components/questions/OtherQuestionList';
+import ProfilesAvatarConnection from '../components/ui/ProfilesAvatarConnection';
 import AuthenticatedComponent from '../components/AuthenticatedComponent';
 import connectToStores from '../utils/connectToStores';
 import UserStore from '../stores/UserStore';
+import StatsStore from '../stores/StatsStore';
 import ProfileStore from '../stores/ProfileStore';
 import QuestionStore from '../stores/QuestionStore';
 import QuestionsByUserIdStore from '../stores/QuestionsByUserIdStore';
@@ -27,6 +29,7 @@ function requestData(props) {
     const otherUserId = parseInt(params.userId);
     UserActionCreators.requestUser(otherUserId, ['username', 'email', 'picture', 'status']);
     QuestionActionCreators.requestComparedQuestions(userId, otherUserId);
+    UserActionCreators.requestStats(otherUserId);
 }
 
 /**
@@ -38,6 +41,7 @@ function getState(props) {
     const otherUserId = parseInt(props.params.userId);
     const currentUser = UserStore.get(currentUserId);
     const otherUser = UserStore.get(otherUserId);
+    const otherUserStats = StatsStore.get(otherUserId);
     const questions = QuestionStore.get(currentUserId);
     const otherQuestions = QuestionStore.get(otherUserId);
     const pagination = QuestionStore.getPagination(otherUserId);
@@ -48,11 +52,12 @@ function getState(props) {
         otherQuestions,
         questions,
         user,
-        otherUser
+        otherUser,
+        otherUserStats
     };
 }
 
-@connectToStores([UserStore, ProfileStore, QuestionStore, QuestionsByUserIdStore], getState)
+@connectToStores([UserStore, StatsStore, QuestionStore, QuestionsByUserIdStore], getState)
 export default AuthenticatedComponent(class OtherQuestionsPage extends Component {
     static propTypes = {
         // Injected by React Router:
@@ -65,6 +70,7 @@ export default AuthenticatedComponent(class OtherQuestionsPage extends Component
         otherQuestions: PropTypes.object,
         pagination: PropTypes.object,
         otherUser: PropTypes.object,
+        otherUserStats: PropTypes.object,
         // Injected by AuthenticatedComponent
         user: PropTypes.object
     };
@@ -95,6 +101,11 @@ export default AuthenticatedComponent(class OtherQuestionsPage extends Component
                 <RegularTopNavbar leftText={'Cancelar'} centerText={this.props.otherUser.username}/>
                 <div data-page="index" className="page other-questions-page">
                     <div id="page-content" className="other-questions-content">
+                        <div className="other-questions-header-container">
+                            <ProfilesAvatarConnection ownPicture={ownPicture} otherPicture={otherPicture} />
+                            {/** TODO: Request from stats (must be included from brain) */}
+                            <div className="other-questions-stats-title">20 Coincidencias</div>
+                        </div>
                         <OtherQuestionList otherQuestions={this.props.otherQuestions} questions={this.props.questions} userId={this.props.user.qnoow_id} ownPicture={ownPicture} otherPicture={otherPicture} />
                         <div className="loading-gif" style={{display: this.props.pagination.nextLink}}></div>
                         <br />

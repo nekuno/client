3c6f1be7510eb4aaf0a2edaa1817d0f3bc93a55d
Manolo Salsas <manolez@gmail.com>
2016-01-26 12:46:06 +0100

    Fix answer inputs behaviour

--- src/js/components/ui/InputRadio.js
+++ src/js/components/ui/InputRadio.js
@@ -6,6 +6,7 @@ export default class InputRadio extends Component {
 		name: PropTypes.string.isRequired,
 		text: PropTypes.string.isRequired,
 		checked: PropTypes.bool.isRequired,
+		defaultChecked: PropTypes.bool.isRequired,
 		onClickHandler: PropTypes.func.isRequired
 	};
 
@@ -13,10 +14,6 @@ export default class InputRadio extends Component {
 		super(props);
 
 		this.onClickHandler = this.onClickHandler.bind(this);
-
-		this.state = {
-			checked: props.checked
-		}
 	}
 
 	render() {
@@ -25,7 +22,7 @@ export default class InputRadio extends Component {
 				<div className="item-inner">
 					<div className="item-title">{this.props.text}</div>
 				</div>
-				<input type="radio" name={this.props.name} value={this.props.value} checked={this.state.checked} defaultChecked={this.props.checked} />
+				<input type="radio" name={this.props.name} value={this.props.value} checked={this.props.checked} defaultChecked={this.props.defaultChecked} readOnly/>
 				<div className="item-media" onClick={this.onClickHandler}>
 					<i className="icon icon-form-checkbox"></i>
 				</div>
@@ -34,7 +31,9 @@ export default class InputRadio extends Component {
 	}
 
 	onClickHandler() {
-		this.setState({'checked': true});
-		this.props.onClickHandler(this.props.value);
+		let _self = this;
+		setTimeout(function () {
+			_self.props.onClickHandler(_self.props.value);
+		}, 50);
 	}
 }

dcea46ce2abfe34878381b9e341945961f0b52ad
Juan Luis Martínez <juanlu@comakai.com>
2016-02-01 11:42:17 +0100

    QS-913: Autologin

--- src/js/actions/LoginActionCreators.js
+++ src/js/actions/LoginActionCreators.js
@@ -2,27 +2,31 @@ import { dispatch, dispatchAsync } from '../dispatcher/Dispatcher';
 import ActionTypes from '../constants/ActionTypes';
 import AuthService from '../services/AuthService';
 
-export default {
+export default new class LoginActionCreators {
 
-    loginUser: (username, password) => {
+    loginUser(username, password) {
         let promise = AuthService.login(username, password);
         dispatchAsync(promise, {
             request: ActionTypes.REQUEST_LOGIN_USER,
             success: ActionTypes.REQUEST_LOGIN_USER_SUCCESS,
             failure: ActionTypes.REQUEST_LOGIN_USER_ERROR
         }, {username, password});
-    },
+    }
 
-    register: (user, profile) => {
+    register(user, profile) {
         let promise = AuthService.register(user, profile);
         dispatchAsync(promise, {
             request: ActionTypes.REQUEST_REGISTER_USER,
             success: ActionTypes.REQUEST_REGISTER_USER_SUCCESS,
             failure: ActionTypes.REQUEST_REGISTER_USER_ERROR
-        }, {user, profile});
-    },
+        }, {user, profile})
+            .then(() => {
+                return this.loginUser(user.username, user.plainPassword);
+            }, () => {
+            });
+    }
 
-    logoutUser: () => {
+    logoutUser() {
         dispatch(ActionTypes.LOGOUT_USER);
     }
 }

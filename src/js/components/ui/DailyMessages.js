caf8e0124a14127e9c3117939c43fdc1e8358e15
Juan Luis Martínez <juanlu@comakai.com>
2016-03-09 10:49:04 +0100

    QS-908: Messages

--- src/js/components/ui/DailyMessages.js
+++ src/js/components/ui/DailyMessages.js
@@ -3,30 +3,26 @@ import Message from './Message';
 import moment from 'moment';
 
 export default class DailyMessages extends Component {
-	static propTypes = {
-		messages: PropTypes.array.isRequired,
-		ownPicture: PropTypes.string.isRequired,
-		otherPicture: PropTypes.string.isRequired,
-		date: PropTypes.string.isRequired
-	};
 
-	render() {
-		return (
-			<div>
-				<div className="daily-message-title">
-					{moment(this.props.date).format('dddd, MMMM Do YYYY')}
-				</div>
-				{this.props.messages.map((message, index) => {
-					return (
-						message.isOwnMessage ?
-							<Message key={index} {...message} picture={this.props.ownPicture} rightPicture={true}/>
-							:
-							<Message key={index} {...message} picture={this.props.otherPicture} rightPicture={false}/>
-					);
-				})}
-				<br />
-				<br />
-			</div>
-		);
-	}
+    static propTypes = {
+        messages: PropTypes.array.isRequired,
+        date    : PropTypes.string
+    };
+
+    render() {
+        return (
+            <div>
+                <div className="daily-message-title">
+                    {moment(this.props.date).format('dddd, MMMM Do YYYY')}
+                </div>
+                {
+                    this.props.messages.map((message) => {
+                        return <Message key={message.id} message={message} />;
+                    })
+                }
+                <br />
+                <br />
+            </div>
+        );
+    }
 }

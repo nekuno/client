55011b643ccbbd80a209391116ac94a65c0a51f4
Juan Luis Martínez <juanlu@comakai.com>
2016-03-10 12:25:49 +0100

    QS-908: Send on keydown enter

--- src/js/components/ui/MessagesToolBar.js
+++ src/js/components/ui/MessagesToolBar.js
@@ -9,10 +9,23 @@ export default class MessagesToolBar extends Component {
     constructor(props) {
         super(props);
         this.handleClick = this.handleClick.bind(this);
+        this._onKeyDown = this._onKeyDown.bind(this);
     }
 
-    handleClick(event) {
-        this.props.onClickHandler(this.refs.textarea.value, event);
+    handleClick() {
+        let text = this.refs.textarea.value.trim();
+        if (text) {
+            this.props.onClickHandler(text);
+        }
+        this.refs.textarea.value = '';
+    }
+
+    _onKeyDown(event) {
+        let ENTER_KEY_CODE = 13;
+        if (event.keyCode === ENTER_KEY_CODE) {
+            event.preventDefault();
+            this.handleClick();
+        }
     }
 
     render() {
@@ -21,7 +34,7 @@ export default class MessagesToolBar extends Component {
                 <div className="toolbar-inner">
                     <div className="toolbar messagebar">
                         <div className="toolbar-inner">
-                            <textarea placeholder="Escribir mensaje..." ref="textarea" />
+                            <textarea placeholder="Escribir mensaje..." ref="textarea" onKeyDown={this._onKeyDown}/>
                             <a onClick={this.handleClick}>Enviar</a>
                         </div>
                     </div>

4e05a28ddba1daae7f705f3b5746c16394aabd18
Juan Luis Martínez <juanlu@comakai.com>
2016-03-14 12:02:07 +0100

    QS-908: Hide day for now

--- src/js/components/ui/DailyMessages.js
+++ src/js/components/ui/DailyMessages.js
@@ -12,9 +12,11 @@ export default class DailyMessages extends Component {
     render() {
         return (
             <div>
+                {/*
                 <div className="daily-message-title">
                     {moment(this.props.date).format('dddd, MMMM Do YYYY')}
                 </div>
+                */}
                 {
                     this.props.messages.map((message) => {
                         return <Message key={message.id} message={message} />;

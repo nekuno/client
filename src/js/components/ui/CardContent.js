7fc819eb60dde3f115615bdd56452be9c8984492
Manolo Salsas <manolez@gmail.com>
2016-04-13 09:34:38 +0200

    QS-959: Use fixed height image for interests list

--- src/js/components/ui/CardContent.js
+++ src/js/components/ui/CardContent.js
@@ -39,6 +39,7 @@ export default class CardContent extends Component {
 		matching: PropTypes.number,
 		rate: PropTypes.bool,
 		hideLikeButton: PropTypes.bool.isRequired,
+		fixedHeight: PropTypes.bool,
 		loggedUserId: PropTypes.number.isRequired,
 		onClickHandler: PropTypes.func
 	};
@@ -51,22 +52,24 @@ export default class CardContent extends Component {
 	}
 
 	render() {
-		let title = this.props.title ? <div>{this.props.title.substr(0, 20)}{this.props.title.length > 20 ? '...' : ''}</div> : 'Link';
-		let subTitle = this.props.description ? <div>{this.props.description.substr(0, 20)}{this.props.description.length > 20 ? '...' : ''}</div> : '';
-		let likeButtonText = this.props.rate ? 'Quitar' : 'Me interesa';
-		let likeButton = this.props.hideLikeButton ? '' : <div className="like-button-container"><Button {...this.props} onClick={this.onRate}>{likeButtonText}</Button></div>;
+		const {title, description, types, rate, hideLikeButton, fixedHeight, thumbnail, url, matching} = this.props;
+		const cardTitle = title ? <div>{title.substr(0, 20)}{title.length > 20 ? '...' : ''}</div> : 'Link';
+		const subTitle =description ? <div>{description.substr(0, 20)}{description.length > 20 ? '...' : ''}</div> : '';
+		const likeButtonText = rate ? 'Quitar' : 'Me interesa';
+		const likeButton = hideLikeButton ? '' : <div className="like-button-container"><Button {...this.props} onClick={this.onRate}>{likeButtonText}</Button></div>;
+		const imageClass = fixedHeight ? 'image fixed-height-image' : 'image';
 		let imgSrc = 'img/default-content-image.jpg';
-		if (this.props.thumbnail) {
-			imgSrc = this.props.thumbnail;
-		} else if (this.props.types.indexOf('Image') > -1) {
-			imgSrc = this.props.url;
+		if (thumbnail) {
+			imgSrc = thumbnail;
+		} else if (types.indexOf('Image') > -1) {
+			imgSrc = url;
 		}
 		return (
 			<div className="card person-card">
 				<div className="card-header" onClick={this.onClickHandler}>
 					<a>
 						<div className="card-title">
-							{title}
+							{cardTitle}
 						</div>
 					</a>
 					<div className="card-sub-title">
@@ -74,19 +77,19 @@ export default class CardContent extends Component {
 					</div>
 				</div>
 				<div className="card-icons" onClick={this.onClickHandler}>
-					<CardIcons types={this.props.types}/>
+					<CardIcons types={types}/>
 				</div>
 				<div className="card-content" onClick={this.onClickHandler}>
 					<div className="card-content-inner">
 						<a>
-							<div className="image">
+							<div className={imageClass}>
 								<img src={imgSrc} />
 							</div>
 						</a>
-						{typeof this.props.matching !== 'undefined' ?
+						{typeof matching !== 'undefined' ?
 							<div className="matching">
-								<div className="matching-value">Compatibilidad {this.props.matching}%</div>
-								<ProgressBar percentage={this.props.matching} />
+								<div className="matching-value">Compatibilidad {matching}%</div>
+								<ProgressBar percentage={matching} />
 							</div>
 							:
 							''

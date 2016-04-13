4a5342e474d6e1963040ff8442997bbe63466885
Juan Luis Martínez <juanlu@comakai.com>
2016-04-13 22:03:33 +0200

    QS-978: Recommendation components refactor

--- src/js/components/recommendations/RecommendationUser.js
+++ src/js/components/recommendations/RecommendationUser.js
@@ -1,13 +1,12 @@
 import React, { PropTypes, Component } from 'react';
-import shouldPureComponentUpdate from '../../../../node_modules/react-pure-render/function';
 import CardUser from './../ui/CardUser';
 
 export default class RecommendationUser extends Component {
     static propTypes = {
         recommendation: PropTypes.object.isRequired,
-        last: PropTypes.bool.isRequired,
-        accessibleKey: PropTypes.number.isRequired,
-        userId: PropTypes.number.isRequired
+        last          : PropTypes.bool.isRequired,
+        accessibleKey : PropTypes.number.isRequired,
+        userId        : PropTypes.number.isRequired
     };
 
     render() {
@@ -18,7 +17,17 @@ export default class RecommendationUser extends Component {
         return (
             <div className="swiper-slide">
                 <div className={'recommendation recommendation-' + key}>
-                    <CardUser loggedUserId={this.props.userId} userId={recommendation.id} username={recommendation.username} location={recommendation.location} canSendMessage={true} picture={recommendation.picture} matching={Math.round(recommendation.similarity * 100)} liked={liked} hideLikeButton={false} />
+                    <CardUser
+                        loggedUserId={this.props.userId}
+                        userId={recommendation.id}
+                        username={recommendation.username}
+                        location={recommendation.location}
+                        canSendMessage={true}
+                        picture={recommendation.picture}
+                        matching={Math.round(recommendation.similarity * 100)}
+                        liked={liked}
+                        hideLikeButton={false}
+                    />
                 </div>
             </div>
         );
--- src/js/components/recommendations/RecommendationsTopNavbar.js
+++ src/js/components/recommendations/RecommendationsTopNavbar.js
@@ -1,27 +1,24 @@
 import React, { PropTypes, Component } from 'react';
-import shouldPureComponentUpdate from 'react-pure-render/function';
 import TopLeftIcon from '../ui/TopLeftIcon';
 import RegularTopTitle from '../ui/RegularTopTitle';
 import TopRightRecommendationIcons from '../ui/TopRightRecommendationIcons';
 
 export default class RecommendationsTopNavbar extends Component {
-	static propTypes = {
-		centerText: PropTypes.string
-	};
+    static propTypes = {
+        centerText: PropTypes.string
+    };
 
-	shouldComponentUpdate = shouldPureComponentUpdate;
-
-	render() {
-		return (
-			<div className="navbar">
-				<div id="navbar-inner" className="navbar-inner">
-					<div className="row">
-						<TopLeftIcon icon={'left-arrow'} />
-						<RegularTopTitle text={this.props.centerText} />
-						<TopRightRecommendationIcons />
-					</div>
-				</div>
-			</div>
-		);
-	}
+    render() {
+        return (
+            <div className="navbar">
+                <div id="navbar-inner" className="navbar-inner">
+                    <div className="row">
+                        <TopLeftIcon icon={'left-arrow'}/>
+                        <RegularTopTitle text={this.props.centerText}/>
+                        <TopRightRecommendationIcons />
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }

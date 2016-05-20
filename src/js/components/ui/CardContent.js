a61b2378f229f48cf54f86ccfef4aa1a1e5f64b0
Roberto Martinez <yawmoght@gmail.com>
2016-05-20 15:29:56 +0200

    QS-1001: Display empty title when no recommendation title is available

--- src/js/components/ui/CardContent.js
+++ src/js/components/ui/CardContent.js
@@ -53,7 +53,7 @@ export default class CardContent extends Component {
 
     render() {
         const {title, description, types, rate, hideLikeButton, fixedHeight, thumbnail, url, matching} = this.props;
-        const cardTitle = title ? <div>{title.substr(0, 20)}{title.length > 20 ? '...' : ''}</div> : <div>Link</div>;
+        const cardTitle = title ? <div>{title.substr(0, 20)}{title.length > 20 ? '...' : ''}</div> : <div></div>;
         const subTitle = description ? <div>{description.substr(0, 20)}{description.length > 20 ? '...' : ''}</div> : fixedHeight ? <div>&nbsp;</div> : '';
         const likeButtonText = rate ? 'Quitar' : 'Me interesa';
         const likeButton = hideLikeButton ? '' : <div className="like-button-container"><Button {...this.props} onClick={this.onRate}>{likeButtonText}</Button></div>;

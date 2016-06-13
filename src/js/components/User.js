841e2688278ce03d637c0aeb3fddbf33293218b5
Juan Luis Martínez <juanlu@comakai.com>
2016-06-13 18:18:19 +0200

    QS-1030: Fix error on iOS with profile data.

--- src/js/components/User.js
+++ src/js/components/User.js
@@ -11,21 +11,20 @@ export default class User extends Component {
     shouldComponentUpdate = shouldPureComponentUpdate;
 
     render() {
-        const { user, profile } = this.props;
+        const {user, profile} = this.props;
         let imgSrc = user.picture ? `${IMAGES_ROOT}media/cache/user_avatar_180x180/user/images/${user.picture}` : `${IMAGES_ROOT}media/cache/user_avatar_180x180/bundles/qnoowweb/images/user-no-img.jpg`;
         return (
             <div className="User">
                 <div className="content-block user-block">
                     <div className="user-image">
-                        <img src={imgSrc} />
+                        <img src={imgSrc}/>
                     </div>
                     <div className="user-data">
                         <div className="user-username title">
                             {user.username}
                         </div>
                         <div className="user-location">
-                            {/** TODO: For some reason, profile is different for different renders. This is a temporal fix */}
-                            <span className="icon-marker"></span> {selectn('Location', profile) || selectn('location.locality', profile) || selectn('location.address', profile) || ''}
+                            <span className="icon-marker"></span> {selectn('location.locality', profile) || selectn('location.address', profile) || ''}
                         </div>
                     </div>
                 </div>

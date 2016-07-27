3d753ecf05073fc0d4c9affa4c1b9706152201e9
Manolo Salsas <manolez@gmail.com>
2016-07-27 15:00:56 +0200

    QS-1056: Add gender field as question

---
+++ src/js/components/fieldsQuestions/profileFields/GenderField.js
@@ -0,0 +1,142 @@
+import React, { PropTypes, Component } from 'react';
+import TextRadios from '../../ui/TextRadios';
+import InputCheckbox from '../../ui/InputCheckbox';
+import FullWidthButton from '../../ui/FullWidthButton';
+import translate from '../../../i18n/Translate';
+import selectn from 'selectn';
+
+@translate('GenderField')
+export default class GenderField extends Component {
+    static propTypes = {
+        gender         : PropTypes.string,
+        metadata       : PropTypes.object,
+        onSaveHandler  : PropTypes.func,
+        // Injected by @translate:
+        strings        : PropTypes.object
+    };
+
+    constructor(props) {
+        super(props);
+        this.onClickGender = this.onClickGender.bind(this);
+        this.onClickDescriptiveGender = this.onClickDescriptiveGender.bind(this);
+        this.onClickShowDescriptiveGender = this.onClickShowDescriptiveGender.bind(this);
+        this.state = {
+            gender               : props.gender,
+            descriptiveGender    : [],
+            showDescriptiveGender: false
+        };
+    }
+    
+    handleClickSave() {
+        this.props.onSaveHandler(this.state.gender, this.state.descriptiveGender);
+    }
+
+    onClickGender(gender) {
+        this.setState({
+            gender: gender
+        })
+    }
+
+    onClickDescriptiveGender(checked, value) {
+
+        let descriptiveGender = this.state.descriptiveGender;
+        if (descriptiveGender.length === 5 && checked && descriptiveGender.indexOf(value) === -1) {
+            nekunoApp.alert(this.props.strings.maxDescriptiveGender);
+        } else {
+            if (checked) {
+                descriptiveGender.push(value);
+            } else {
+                descriptiveGender = descriptiveGender.filter(val => val !== value);
+            }
+        }
+        this.setState({
+            descriptiveGender: descriptiveGender
+        });
+    }
+
+    onClickShowDescriptiveGender() {
+        this.setState(
+            {showDescriptiveGender: !this.state.showDescriptiveGender}
+        );
+    }
+    
+    render() {
+        const {metadata, strings} = this.props;
+        const descriptiveGenderChoices = selectn('descriptiveGender.choices', metadata) || {};
+        const descriptiveGenderChoicesLength = Object.keys(descriptiveGenderChoices).length || 0;
+        let descriptiveGenderFirstColumnCounter = 0;
+        let descriptiveGenderSecondColumnCounter = 0;
+        return (
+            <div>
+                <div className="answer-question">
+                    <div className="title answer-question-title">
+                        {strings.title}
+                    </div>
+                    <TextRadios title={this.state.descriptiveGender.length > 0 ? strings.include : strings.gender} labels={[
+						{key: 'male', text: strings.male},
+						{key: 'female', text: strings.female}
+					]} onClickHandler={this.onClickGender} value={this.state.gender}/>
+                    <div style={{textAlign: 'center', marginBottom: '20px'}}>
+                        <a onClick={this.onClickShowDescriptiveGender}>{ this.state.showDescriptiveGender ? strings.hideDescriptiveGender : strings.showDescriptiveGender}</a>
+                    </div>
+                    {this.state.showDescriptiveGender && descriptiveGenderChoices ?
+                        <div className="list-block">
+                            <ul className="checkbox-genders-list">
+                                {Object.keys(descriptiveGenderChoices).map((id) => {
+                                    descriptiveGenderFirstColumnCounter++;
+                                    if (descriptiveGenderFirstColumnCounter > descriptiveGenderChoicesLength / 2) {
+                                        return '';
+                                    }
+                                    let text = metadata.descriptiveGender.choices[id];
+                                    let checked = this.state.descriptiveGender.indexOf(id) !== -1;
+                                    return (
+                                        <li key={id}>
+                                            <InputCheckbox value={id} name={'descriptiveGender[]'} text={text} checked={checked} onClickHandler={this.onClickDescriptiveGender} reverse={true}/>
+                                        </li>
+                                    )
+                                })}
+                            </ul>
+                            <ul className="checkbox-genders-list">
+                                {Object.keys(descriptiveGenderChoices).map((id) => {
+                                    descriptiveGenderSecondColumnCounter++;
+                                    if (descriptiveGenderSecondColumnCounter <= descriptiveGenderChoicesLength / 2) {
+                                        return '';
+                                    }
+                                    let text = metadata.descriptiveGender.choices[id];
+                                    let checked = this.state.descriptiveGender.indexOf(id) !== -1;
+                                    return (<li key={id}>
+                                        <InputCheckbox value={id} name={'descriptiveGender[]'} text={text} checked={checked} onClickHandler={this.onClickDescriptiveGender} reverse={true}/>
+                                    </li>)
+                                })}
+                            </ul>
+                        </div>
+
+                        :
+                        ''
+                    }
+                </div>
+                <br />
+                <br />
+                <FullWidthButton type="submit" onClick={this.handleClickSave.bind(this)}>{strings.save}</FullWidthButton>
+                <br />
+                <br />
+                <br />
+                <br />
+            </div>
+        );
+    }
+}
+
+GenderField.defaultProps = {
+    strings: {
+        include              : 'Include on searches as',
+        gender               : 'Gender',
+        male                 : 'Male',
+        female               : 'Female',
+        showDescriptiveGender: 'Show other genders',
+        hideDescriptiveGender: 'Hide other genders',
+        maxDescriptiveGender : 'The maximum number of options permitted is 5, uncheck any other options to choose this one',
+        title                : 'Select your gender',
+        save                 : 'Save'
+    }
+};
--- src/js/pages/AnswerProfileFieldPage.js
+++ src/js/pages/AnswerProfileFieldPage.js
@@ -3,6 +3,7 @@ import TopNavBar from '../components/ui/TopNavBar';
 import EmptyMessage from '../components/ui/EmptyMessage';
 import BirthdayField from '../components/fieldsQuestions/profileFields/BirthdayField';
 import LocationField from '../components/fieldsQuestions/profileFields/LocationField';
+import GenderField from '../components/fieldsQuestions/profileFields/GenderField';
 import AuthenticatedComponent from '../components/AuthenticatedComponent';
 import translate from '../i18n/Translate';
 import connectToStores from '../utils/connectToStores';
@@ -23,6 +24,9 @@ function requestData(props) {
     if (!ProfileStore.get(parseId(props.user))) {
         UserActionCreators.requestOwnProfile(parseId(props.user));
     }
+    if (!ProfileStore.getMetadata()) {
+        UserActionCreators.requestMetadata();
+    }
 }
 
 /**
@@ -31,6 +35,7 @@ function requestData(props) {
 function getState(props) {
     const userId = parseId(props.user);
     const profile = ProfileStore.get(userId);
+    const metadata = ProfileStore.getMetadata();
     const errors = ProfileStore.getErrors();
     const registerQuestionsLength = QuestionStore.registerQuestionsLength();
     const answersLength = QuestionStore.answersLength(userId);
@@ -46,6 +51,7 @@ function getState(props) {
 
     return {
         profile,
+        metadata,
         errors,
         nextProfileField,
         profileQuestionsComplete,
@@ -69,6 +75,8 @@ export default class AnswerProfileFieldPage extends Component {
         // Injected by @translate:
         strings                 : PropTypes.object,
         // Injected by @connectToStores:
+        profile                 : PropTypes.object,
+        metadata                : PropTypes.object,
         errors                  : PropTypes.string,
         nextProfileField        : PropTypes.object,
         profileQuestionsComplete: PropTypes.bool,
@@ -84,6 +92,7 @@ export default class AnswerProfileFieldPage extends Component {
         super(props);
 
         this.handleClickSave = this.handleClickSave.bind(this);
+        this.handleClickGenderSave = this.handleClickGenderSave.bind(this);
     }
 
     componentWillMount() {
@@ -105,16 +114,26 @@ export default class AnswerProfileFieldPage extends Component {
         profile[fieldName] = data;
         UserActionCreators.editProfile(profile);
     }
+
+    handleClickGenderSave(gender, descriptiveGender) {
+        if (!gender) {
+            nekunoApp.alert(this.props.strings.genderEmpty);
+            return null;
+        }
+        let profile = Object.assign({}, ProfileStore.get(parseId(this.props.user)));
+        profile['gender'] = gender;
+        profile['descriptiveGender'] = descriptiveGender;
+        UserActionCreators.editProfile(profile);
+    }
     
     render() {
 
-        const {profile, strings, nextProfileField, totalQuestions, questionNumber} = this.props;
-        console.log(profile)
+        const {profile, metadata, strings, nextProfileField, totalQuestions, questionNumber} = this.props;
         const navBarTitle = typeof profile != 'undefined' ?
             strings.question + ' ' + questionNumber + '/' + totalQuestions
             : '';
         let fieldToRender = null;
-        const nextProfileFiledName = nextProfileField ? nextProfileField.type : null;
+        const nextProfileFiledName = nextProfileField ? nextProfileField.name : null;
         switch (nextProfileFiledName) {
             case 'birthday':
                 fieldToRender = <BirthdayField birthday={profile && profile.birthday ? profile.birthday : ''} onSaveHandler={this.handleClickSave} />;
@@ -122,6 +141,10 @@ export default class AnswerProfileFieldPage extends Component {
 
             case 'location':
                 fieldToRender = <LocationField location={profile && profile.location ? profile.location : ''} onSaveHandler={this.handleClickSave} />;
+                break;
+
+            case 'gender':
+                fieldToRender = <GenderField gender={profile && profile.gender ? profile.gender : null} metadata={metadata} onSaveHandler={this.handleClickGenderSave} />;
                 break;
             
             default:
@@ -143,6 +166,7 @@ export default class AnswerProfileFieldPage extends Component {
 AnswerProfileFieldPage.defaultProps = {
     strings: {
         question      : 'Question',
-        loadingMessage: 'Loading questions'
+        loadingMessage: 'Loading questions',
+        genderEmpty   : 'Gender is not selected'
     }
 };

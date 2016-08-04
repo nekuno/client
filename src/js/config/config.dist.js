5dc5f12f77e9635bc8c0dd70ca0d4beef44416db
Manolo Salsas <manolez@gmail.com>
2016-08-04 08:55:08 +0200

    Fix config.dist

--- src/js/config/config.dist.js
+++ src/js/config/config.dist.js
@@ -5,10 +5,10 @@ export default {
     FACEBOOK_ID      : 525923770862645,
     FACEBOOK_SCOPE   : 'public_profile,email,user_birthday,user_location,user_likes,user_posts,photos,offline_access',
     TWITTER_ID       : '3tn60KgFss8KfVAmCujtUY8vg',
-    TWITTER_SCOPE    : 'offline_access',
+    TWITTER_SCOPE    : '',
     GOOGLE_ID        : '351352065100-hjnd2afbiirca4nihu9ck461nv14b5l1.apps.googleusercontent.com',
-    GOOGLE_SCOPE     : 'email,profile,openid,photos,https://www.googleapis.com/auth/youtube.readonly',
+    GOOGLE_SCOPE     : 'email,profile,openid,photos,offline_access,https://www.googleapis.com/auth/youtube.readonly',
     SPOTIFY_ID       : 'e8c3557e29ad4974a1f41e93aeaf6e79',
-    SPOTIFY_SCOPE    : 'user-read-private user-read-email playlist-read-private offline_access',
+    SPOTIFY_SCOPE    : 'user-read-private user-read-email playlist-read-private',
     LAST_RELEASE_DATE: '1 de marzo de 2020'
 }

62fe1803a95ba975431fa884b5e92d1fc6c93fa2
Juan Luis Martínez <juanlu@comakai.com>
2016-08-04 14:33:44 +0200

    Add user-read-birthdate to dist.js

--- src/js/config/config.dist.js
+++ src/js/config/config.dist.js
@@ -9,6 +9,6 @@ export default {
     GOOGLE_ID        : '351352065100-hjnd2afbiirca4nihu9ck461nv14b5l1.apps.googleusercontent.com',
     GOOGLE_SCOPE     : 'email,profile,openid,photos,https://www.googleapis.com/auth/youtube.readonly,offline_access',
     SPOTIFY_ID       : 'e8c3557e29ad4974a1f41e93aeaf6e79',
-    SPOTIFY_SCOPE    : 'user-read-private user-read-email playlist-read-private',
+    SPOTIFY_SCOPE    : 'user-read-private user-read-email playlist-read-private user-read-birthdate',
     LAST_RELEASE_DATE: '1 de marzo de 2020'
 }

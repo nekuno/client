857f3186822594fbee8eee537da564b40fe921a6
msalsas <manolez@gmail.com>
2016-11-22 11:45:20 +0100

    Refactor LikeStore

--- src/js/stores/LikeStore.js
+++ src/js/stores/LikeStore.js
@@ -1,75 +1,76 @@
-import { register, waitFor } from '../dispatcher/Dispatcher';
-import { createStore } from '../utils/StoreUtils';
+import { waitFor } from '../dispatcher/Dispatcher';
 import UserStore from '../stores/UserStore';
 import ActionTypes from '../constants/ActionTypes';
 import selectn from 'selectn';
+import BaseStore from './BaseStore';
 
-let _likes = [];
+class LikeStore extends BaseStore {
+    setInitial() {
+        this. _likes = [];
+    }
 
-const LikeStore = createStore({
-    contains(userId1, userId2) {
-        return _likes.some(like => like.from == userId1 && like.to == userId2);
-    },
-
-    get(userId1, userId2) {
-        const like = _likes.find(like => like.from == userId1 && like.to == userId2) || {value: 0};
-        return like.value;
-    },
-
-    merge(userId1, userId2, value) {
-        if (_likes.some(like => like.from == userId1 && like.to == userId2)) {
-            const index = _likes.findIndex(like => like.from == userId1 && like.to == userId2);
-            _likes[index] = {from: userId1, to: userId2, value: value};
-        } else {
-            _likes.push({from: userId1, to: userId2, value: value});
+    _registerToActions(action) {
+        waitFor([UserStore.dispatchToken]);
+        const {from, to} = action;
+        switch (action.type) {
+            case ActionTypes.LIKE_USER:
+                this.merge(from, to, null);
+                this.emitChange();
+                break;
+            case ActionTypes.UNLIKE_USER:
+                this.merge(from, to, null);
+                this.emitChange();
+                break;
+            case ActionTypes.DISLIKE_USER:
+                this.merge(from, to, null);
+                this.emitChange();
+                break;
+            case ActionTypes.IGNORE_USER:
+                this.emitChange();
+                break;
+            case ActionTypes.LIKE_USER_SUCCESS:
+                this.merge(from, to, 1);
+                this.emitChange();
+                break;
+            case ActionTypes.UNLIKE_USER_SUCCESS:
+                this.merge(from, to, 0);
+                this.emitChange();
+                break;
+            case ActionTypes.DISLIKE_USER_SUCCESS:
+                this.merge(from, to, -1);
+                this.emitChange();
+                break;
+            case ActionTypes.IGNORE_USER_SUCCESS:
+                this.emitChange();
+                break;
+            case ActionTypes.REQUEST_LIKE_USER_SUCCESS:
+                const like = selectn('response.result', action) ? 1 : 0;
+                this.merge(from, to, like);
+                this.emitChange();
+                break;
+            case ActionTypes.LOGOUT_USER:
+                this._likes = [];
+                break;
         }
     }
-});
 
-LikeStore.dispatchToken = register(action => {
-
-    waitFor([UserStore.dispatchToken]);
-    const {from, to} = action;
-    switch(action.type) {
-        case ActionTypes.LIKE_USER:
-            LikeStore.merge(from, to, null);
-            LikeStore.emitChange();
-            break;
-        case ActionTypes.UNLIKE_USER:
-            LikeStore.merge(from, to, null);
-            LikeStore.emitChange();
-            break;
-        case ActionTypes.DISLIKE_USER:
-            LikeStore.merge(from, to, null);
-            LikeStore.emitChange();
-            break;
-        case ActionTypes.IGNORE_USER:
-            LikeStore.emitChange();
-            break;
-        case ActionTypes.LIKE_USER_SUCCESS:
-            LikeStore.merge(from, to, 1);
-            LikeStore.emitChange();
-            break;
-        case ActionTypes.UNLIKE_USER_SUCCESS:
-            LikeStore.merge(from, to, 0);
-            LikeStore.emitChange();
-            break;
-        case ActionTypes.DISLIKE_USER_SUCCESS:
-            LikeStore.merge(from, to, -1);
-            LikeStore.emitChange();
-            break;
-        case ActionTypes.IGNORE_USER_SUCCESS:
-            LikeStore.emitChange();
-            break;
-        case ActionTypes.REQUEST_LIKE_USER_SUCCESS:
-            const like = selectn('response.result', action) ? 1 : 0;
-            LikeStore.merge(from, to, like);
-            LikeStore.emitChange();
-            break;
-        case ActionTypes.LOGOUT_USER:
-            _likes = [];
-            break;
+    contains(userId1, userId2) {
+        return this._likes.some(like => like.from == userId1 && like.to == userId2);
     }
-});
 
-export default LikeStore;
+    get(userId1, userId2) {
+        const like = this._likes.find(like => like.to == userId2 && like.from == userId1) || {value: 0};
+        return like.value;
+    }
+
+    merge(userId1, userId2, value) {
+        const index = this._likes.findIndex(like => like.to == userId2 && like.from == userId1);
+        if (index !== -1) {
+            this._likes[index] = {from: userId1, to: userId2, value: value};
+        } else {
+            this._likes.push({from: userId1, to: userId2, value: value});
+        }
+    }
+}
+
+export default new LikeStore();

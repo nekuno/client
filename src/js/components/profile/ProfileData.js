ddca12d88e793aa5a516e4bf032270cd9f54049d
Juan Luis Martínez <juanlu@comakai.com>
2016-12-01 17:54:50 +0100

    QS-1189: split

--- src/js/components/profile/ProfileData.js
+++ src/js/components/profile/ProfileData.js
@@ -16,6 +16,7 @@ export default class ProfileData extends Component {
     constructor(props) {
         super(props);
         this.toggleSeeMore = this.toggleSeeMore.bind(this);
+        this.split = this.split.bind(this);
         this.state = {
             seeMore: props.value.length < maxLength
         };
@@ -23,6 +24,14 @@ export default class ProfileData extends Component {
 
     toggleSeeMore() {
         this.setState({seeMore: !this.state.seeMore});
+    }
+
+    split(text) {
+        return text.split("\n").map(function(item, key) {
+            return (
+                key + 1 === text.split("\n").length ? <span>{item}</span> : <span>{item}<br/></span>
+            )
+        });
     }
 
     render() {
@@ -35,13 +44,13 @@ export default class ProfileData extends Component {
                 <div className="profile-data">
                     <div className="profile-data-name-long">{name}</div>
                     <div className="profile-data-value-long">
-                        {seeMore ? value : value.substring(0, maxLength) + '...'}
+                        {seeMore ? this.split(value) : this.split(value.substring(0, maxLength) + '...')}
                         <span className="see-more" onClick={this.toggleSeeMore}> {seeMore ? strings.seeLess : strings.seeMore}</span>
                     </div>
                 </div>
                 :
                 <div className="profile-data">
-                    <span className="profile-data-name">{name}</span> <span className="profile-data-value">{value}</span>
+                    <span className="profile-data-name">{name}</span> <span className="profile-data-value">{this.split(value)}</span>
                 </div>
         );
     }

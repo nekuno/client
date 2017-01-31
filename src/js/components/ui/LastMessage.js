df01b16066464d86bd9e48b8cf5aa1e7d8cad4c1
msalsas <manolez@gmail.com>
2017-01-31 11:36:53 +0100

    QS-1266: Fix LastMessage link to conversations

--- src/js/components/ui/LastMessage.js
+++ src/js/components/ui/LastMessage.js
@@ -35,7 +35,7 @@ export default class LastMessage extends Component {
 
         return (
             <div className="notification">
-                <Link to={`/conversations/${user.slug}`}>
+                <Link to={`/conversations/${user.slug || encodeURI(user.username.toLowerCase())}`}>
                     <div className="notification-picture">
                         <img src={imageSrc}/>
                         {online ? <div className="status-online">Online</div> : ''}

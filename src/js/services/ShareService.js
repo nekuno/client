b77c4abe165d4e453305e97ea0d421b26048a757
msalsas <manolez@gmail.com>
2017-03-14 10:33:31 +0100

    QS-1316: Copy to clipboard when sharing and add copied message

--- src/js/services/ShareService.js
+++ src/js/services/ShareService.js
@@ -1,6 +1,9 @@
 class ShareService {
 
     share(subject, url, successCallback, errorCallback, copiedMessage) {
+        if (typeof document.execCommand == "function") {
+            this.copyToClipboard(url, copiedMessage);
+        }
         if (window.cordova) {
             // this is the complete list of currently supported params you can pass to the plugin (all optional)
             var options = {
@@ -16,11 +19,6 @@ class ShareService {
                 url: url
             }).then(() => successCallback())
                 .catch(error => errorCallback());
-            // TODO: Uncomment to copy url if execCommand is available instead of sharing with FB
-        /*} else if (typeof document.execCommand == "function") {
-            this.copyToClipboard(url);
-            nekunoApp.alert(copiedMessage);
-            successCallback();*/
         } else {
             FB.ui({
                 method: 'share',
@@ -31,13 +29,26 @@ class ShareService {
         }
     }
 
-    copyToClipboard = function(text) {
+    copyToClipboard = function(text, copiedMessage) {
         let aux = document.createElement("input");
         aux.setAttribute("value", text);
         document.body.appendChild(aux);
         aux.select();
         document.execCommand("copy");
         document.body.removeChild(aux);
+        let messageTag = document.createElement("div");
+        messageTag.className = "fixed-top-message";
+        messageTag.innerHTML = copiedMessage;
+        document.body.appendChild(messageTag);
+        setTimeout(() => {
+            messageTag.className += " visible";
+            setTimeout(() => {
+                messageTag.className = "fixed-top-message";
+                setTimeout(() => {
+                    document.body.removeChild(messageTag);
+                }, 1000);
+            }, 2000);
+        }, 100);
     }
 }
 

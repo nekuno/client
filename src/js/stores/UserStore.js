1b3f35f9fe5839c52349e7040caf07a275e997cd
msalsas <manolez@gmail.com>
2017-04-03 13:59:50 +0200

    Refactor user store

--- src/js/stores/UserStore.js
+++ src/js/stores/UserStore.js
@@ -1,56 +1,61 @@
 import ActionTypes from '../constants/ActionTypes';
-import { register } from '../dispatcher/Dispatcher';
-import { createStore, mergeIntoBag, isInBag } from '../utils/StoreUtils';
+import { mergeIntoBag, isInBag } from '../utils/StoreUtils';
+import BaseStore from './BaseStore';
 import selectn from 'selectn';
 
-let _users = {};
-let _error = null;
+class UserStore extends BaseStore {
 
-const UserStore = createStore({
+    setInitial() {
+        this._users = {};
+        this._error = null;
+    }
+
+    _registerToActions(action) {
+        super._registerToActions(action);
+
+        const responseUsers = selectn('response.entities.users', action);
+        switch(action.type) {
+            case ActionTypes.REQUEST_OWN_USER_SUCCESS:
+            case ActionTypes.EDIT_USER_SUCCESS:
+            case ActionTypes.REQUEST_USER_SUCCESS:
+                mergeIntoBag(this._users, responseUsers);
+                this.emitChange();
+                break;
+            case ActionTypes.REQUEST_PUBLIC_USER_ERROR:
+                this._error = action.error.error;
+                this.emitChange();
+                break;
+
+            default:
+                break;
+        }
+    }
+
     contains(userId, fields) {
-        return isInBag(_users, userId, fields);
-    },
+        return isInBag(this._users, userId, fields);
+    }
 
     get(userId) {
-        return _users[userId];
-    },
+        return this._users[userId];
+    }
 
     containsSlug(slug, fields) {
-        const index = Object.keys(_users).find(userId => _users[userId].slug === slug);
-        return isInBag(_users, index, fields);
-    },
+        const index = Object.keys(this._users).find(userId => this._users[userId].slug === slug);
+        return isInBag(this._users, index, fields);
+    }
 
     getBySlug(slug) {
-        const index = Object.keys(_users).find(userId => _users[userId].slug === slug);
-        return index && index > -1 ? _users[index] : null;
-    },
+        const index = Object.keys(this._users).find(userId => this._users[userId].slug === slug);
+        return index && index > -1 ? this._users[index] : null;
+    }
 
     getError() {
-        let error = _error;
-        _error = null;
+        let error = this._error;
+        this._error = null;
 
         return error;
     }
-});
 
-UserStore.dispatchToken = register(action => {
-    const responseUsers = selectn('response.entities.users', action);
-    if (responseUsers) {
-        mergeIntoBag(_users, responseUsers);
-        UserStore.emitChange();
-    }
-    switch(action.type) {
-        case ActionTypes.REQUEST_PUBLIC_USER_ERROR:
-            _error = action.error.error;
-            UserStore.emitChange();
-            break;
-        case ActionTypes.LOGOUT_USER:
-            _users = {};
-            break;
+}
 
-        default:
-            break;
-    }
-});
-
-export default UserStore;
+export default new UserStore();

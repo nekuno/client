85182b4e51385c26ed9acaba87780cea588fc0d7
msalsas <manolez@gmail.com>
2017-06-13 13:30:37 +0200

    QS-1383: Fix back button

--- src/js/actions/RouterActionCreators.js
+++ src/js/actions/RouterActionCreators.js
@@ -4,5 +4,18 @@ import ActionTypes from '../constants/ActionTypes';
 export default {
     storeRouterTransitionPath: (path) => {
         dispatch(ActionTypes.ROUTER_NEXT_TRANSITION_PATH, {path});
+    },
+
+    nextRoute: (route) => {
+        dispatch(ActionTypes.NEXT_ROUTE, {route});
+    },
+
+
+    removePreviousRoute: () => {
+        dispatch(ActionTypes.REMOVE_PREV_ROUTE, {});
+    },
+
+    previousRoute: (route) => {
+        dispatch(ActionTypes.PREVIOUS_ROUTE, {route});
     }
 }
--- src/js/stores/RouterStore.js
+++ src/js/stores/RouterStore.js
@@ -1,10 +1,12 @@
 import ActionTypes from '../constants/ActionTypes';
 import BaseStore from './BaseStore';
+import RouterContainer from '../services/RouterContainer';
 
 class RouterStore extends BaseStore {
 
     setInitial() {
         this._nextPath = null;
+        this._routes = [];
     }
 
     _registerToActions(action) {
@@ -13,6 +15,32 @@ class RouterStore extends BaseStore {
 
             case ActionTypes.ROUTER_NEXT_TRANSITION_PATH:
                 this._nextPath = action.path;
+                break;
+
+            case ActionTypes.NEXT_ROUTE:
+                this._routes.push(action.route);
+                this.emitChange();
+                break;
+
+            case ActionTypes.REMOVE_PREV_ROUTE:
+                this._routes.pop();
+                this.emitChange();
+                break;
+
+            case ActionTypes.PREVIOUS_ROUTE:
+                this._routes.pop();
+                let router = RouterContainer.get();
+                if (this._routes.length > 0) {
+                    const lastRoute = this._routes[this._routes.length - 1];
+                    if (action.route === lastRoute) {
+                        setTimeout(router.replace('discover'), 0);
+                    } else {
+                        router.goBack();
+                    }
+                } else {
+                    setTimeout(router.replace('discover'), 0);
+                }
+                this.emitChange();
                 break;
 
             default:

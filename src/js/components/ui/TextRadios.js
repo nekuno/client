ec508f048b25d74f30d1104b14ad1d12eb2f3419
Roberto Martinez <yawmoght@gmail.com>
2017-07-07 11:01:03 +0200

    QS-1416: Improve disabled TextRadios style

--- src/js/components/ui/TextRadios.js
+++ src/js/components/ui/TextRadios.js
@@ -17,12 +17,27 @@ export default class TextRadios extends Component {
 		super(props);
 	}
 
+	getRadiosClassname(props) {
+        const {className, disabled} = props;
+
+        let finalClassName = 'text-radios';
+        if (className) {
+            finalClassName += ' ' + className;
+        }
+        if (disabled) {
+            finalClassName += ' text-radios-disabled';
+        }
+
+        return finalClassName
+    }
+
 	render() {
-		const {title, labels, value, forceTwoLines, className, disabled} = this.props;
+		const {title, labels, value, forceTwoLines, className} = this.props;
 		let labelsLength = labels.length;
 		let labelsTextLength = 0;
 		labels.forEach(label => labelsTextLength += label.text.length);
 		let showCheckboxesList = !forceTwoLines && (labelsLength > 3 || labelsTextLength > 35);
+		const radiosClassName = this.getRadiosClassname(this.props);
 		return (
 			showCheckboxesList ?
 				<div className={className ? "list-block " + className : "list-block"}>
@@ -36,7 +51,7 @@ export default class TextRadios extends Component {
 					</ul>
 				</div>
 				:
-				<div id="joyride-3-answer-importance" className={className ? "text-radios " + className : "text-radios"}>
+				<div id="joyride-3-answer-importance" className={radiosClassName}>
 					<div className="text-radios-title">{title}</div>
 					<div className={labelsLength ? 'text-radios-container' : ' unique-chip text-radios-container'}>
 						{labels.map(label =>
@@ -44,7 +59,7 @@ export default class TextRadios extends Component {
 								  chipClass={forceTwoLines ? 'chip-two-lines ' + 'chip-' + labelsLength : 'chip-' + labelsLength}
 								  label={label.text}
 								  onClickHandler={this.onClickHandler.bind(this, label.key)}
-								  disabled={value !== label.key || disabled}/>
+								  disabled={value !== label.key}/>
 						)}
 					</div>
 				</div>

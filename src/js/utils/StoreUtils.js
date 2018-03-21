d0be500d1ce98a200a599e808ea32bf1fbfc98da
msalsas <manolez@gmail.com>
2018-03-21 11:58:00 +0100

    Remove old createStore and unused underscore library

--- src/js/utils/StoreUtils.js
+++ src/js/utils/StoreUtils.js
@@ -1,37 +1,5 @@
-import { each, isFunction } from 'underscore';
-import { EventEmitter } from 'events';
 import shallowEqual from 'react-pure-render/shallowEqual';
 import selectn from 'selectn';
-
-const CHANGE_EVENT = 'change';
-
-export function createStore(spec) {
-  const emitter = new EventEmitter();
-  emitter.setMaxListeners(0);
-
-  const store = Object.assign({
-    emitChange() {
-      emitter.emit(CHANGE_EVENT);
-    },
-
-    addChangeListener(callback) {
-      emitter.on(CHANGE_EVENT, callback);
-    },
-
-    removeChangeListener(callback) {
-      emitter.removeListener(CHANGE_EVENT, callback);
-    }
-  }, spec);
-
-  // Auto-bind store methods for convenience
-  each(store, (val, key) => {
-    if (isFunction(val)) {
-      store[key] = store[key].bind(store);
-    }
-  });
-
-  return store;
-}
 
 export function isInBag(bag, id, fields) {
   let item = bag[id];

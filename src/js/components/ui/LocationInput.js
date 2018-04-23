4106e0e9c5ced1e58fbfd20f96dcf2cefafca6c0
msalsas <manolez@gmail.com>
2018-04-23 15:09:21 +0200

    Upgrade react-infinite, fix location input and fix ProfileDataList

--- src/js/components/ui/LocationInput.js
+++ src/js/components/ui/LocationInput.js
@@ -30,30 +30,32 @@ export default class LocationInput extends Component {
 
     onSuggestSelect(suggest) {
         let locality = '', country = '';
-        suggest.gmaps.address_components.forEach(function(component) {
-            component.types.forEach(function(type) {
-                if (!locality && (type === 'locality' || type === 'administrative_area_level_4')) {
-                    locality = component.long_name;
-                }
-                if (!country && type === 'country') {
-                    country = component.long_name;
-                }
+        if (suggest) {
+            suggest.gmaps.address_components.forEach(function(component) {
+                component.types.forEach(function(type) {
+                    if (!locality && (type === 'locality' || type === 'administrative_area_level_4')) {
+                        locality = component.long_name;
+                    }
+                    if (!country && type === 'country') {
+                        country = component.long_name;
+                    }
+                });
             });
-        });
 
-        let location = {
-            latitude : parseFloat(suggest.location.lat),
-            longitude: parseFloat(suggest.location.lng),
-            address  : suggest.label,
-            locality : locality,
-            country  : country
-        };
+            let location = {
+                latitude : parseFloat(suggest.location.lat),
+                longitude: parseFloat(suggest.location.lng),
+                address  : suggest.label,
+                locality : locality,
+                country  : country
+            };
 
-        if (this.props.clearOnSelect) {
-            this.refs['geosuggest'].clear();
+            if (this.props.clearOnSelect) {
+                this.refs['geosuggest'].clear();
+            }
+
+            return this.props.onSuggestSelect(location);
         }
-
-        return this.props.onSuggestSelect(location);
     }
 
     render() {
@@ -69,7 +71,7 @@ export default class LocationInput extends Component {
                             onSuggestSelect={this.onSuggestSelect}
                             getSuggestLabel={function(suggest) { return suggest.description.length > 35 ? suggest.description.slice(0, 35) + '...' : suggest.description }}
                             onFocus={this.onFocusHandler}
-                            skipSuggest={function(suggest) { return suggest.terms.length < 2 || !suggest.types.some(type => type === 'locality') }}
+                            skipSuggest={function(suggest) { return suggest.terms.length < 2 || !suggest.types.some(type => type === 'locality' || type === 'administrative_area_level_4') }}
                             required
                         />
                     </div>

1b643c0d505b720358485ae0fe7c04f3cd7b41e3
albemumo <alberto.munoz@freaksheep.com>
2018-11-22 15:55:17 +0100

    QS-1570 Create ProposalExperience all pages

---
+++ src/js/pages/Proposals/Experience/AvailabilityPage.js
@@ -0,0 +1,259 @@
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import translate from '../../../i18n/Translate';
+import TopNavBar from '../../../components/TopNavBar/TopNavBar.js';
+import '../../../../scss/pages/proposals/experience/availability.scss';
+import StepsBar from "../../../components/ui/StepsBar/StepsBar";
+import connectToStores from "../../../utils/connectToStores";
+import * as ProposalActionCreators from "../../../actions/ProposalActionCreators";
+import Frame from "../../../components/ui/Frame/Frame";
+import RoundedIcon from "../../../components/ui/RoundedIcon/RoundedIcon";
+import CreatingProposalStore from "../../../stores/CreatingProposalStore";
+
+function getState() {
+    const proposal = CreatingProposalStore.proposal;
+    const availability = proposal.availability ? proposal.availability : null;
+
+    return {
+        availability,
+    };
+}
+
+@translate('ProposalsLeisureAvailabilityPage')
+@connectToStores([CreatingProposalStore], getState)
+export default class AvailabilityPage extends Component {
+
+    static propTypes = {
+        // Injected by @translate:
+        strings      : PropTypes.object,
+        // Injected by @connectToStores:
+        availability : PropTypes.object,
+        canContinue  : PropTypes.bool,
+    };
+
+    static contextTypes = {
+        router : PropTypes.object.isRequired
+    };
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            participantLimit : 1,
+            disableSubstract : true,
+        };
+
+        this.topNavBarLeftLinkClick = this.topNavBarLeftLinkClick.bind(this);
+        this.topNavBarRightLinkClick = this.topNavBarRightLinkClick.bind(this);
+        this.onClickAvailabilityHandler = this.onClickAvailabilityHandler.bind(this);
+        this.onClickParticipantsSubstractHandler = this.onClickParticipantsSubstractHandler.bind(this);
+        this.onClickParticipantsPlusHandler = this.onClickParticipantsPlusHandler.bind(this);
+        this.handleStepsBarClick = this.handleStepsBarClick.bind(this);
+    }
+
+    componentWillMount() {
+        if (CreatingProposalStore.proposal.participantLimit) {
+            this.setState({
+                participantLimit : CreatingProposalStore.proposal.participantLimit,
+            });
+            if (CreatingProposalStore.proposal.participantLimit > 1) {
+                this.setState({
+                    disableSubstract : false,
+                });
+            }
+        }
+    }
+
+    topNavBarLeftLinkClick() {
+        this.context.router.push('/proposals-experience-type');
+    }
+
+    topNavBarRightLinkClick() {
+        this.context.router.push('/proposals');
+    }
+
+    onClickAvailabilityHandler() {
+        const proposal = {
+            participantLimit : this.state.participantLimit,
+        };
+        ProposalActionCreators.mergeCreatingProposal(proposal);
+        this.context.router.push('/proposals-experience-availability-dates');
+    }
+
+    onClickParticipantsSubstractHandler() {
+        const newParticipantLimit = this.state.participantLimit - 1;
+        this.setState({
+            participantLimit : newParticipantLimit,
+        });
+
+        if (newParticipantLimit < 2) {
+            this.setState({
+                disableSubstract : true,
+            });
+        }
+    }
+
+    onClickParticipantsPlusHandler() {
+        const newParticipantLimit = this.state.participantLimit + 1;
+        this.setState({
+            participantLimit : newParticipantLimit,
+        });
+
+        if (newParticipantLimit > 1) {
+            this.setState({
+                disableSubstract : false,
+            });
+        }
+    }
+
+    handleStepsBarClick() {
+        const proposal = {
+            participantLimit : this.state.participantLimit,
+        };
+        ProposalActionCreators.mergeCreatingProposal(proposal);
+        this.context.router.push('/proposals-experience-features');
+    }
+
+    render() {
+        const {strings, availability} = this.props;
+        const canContinue = availability !== null;
+        const participantLimit = this.state.participantLimit;
+        const disableSubstract = this.state.disableSubstract;
+
+        const dailyWeekdayOptions = {
+            monday    : strings.monday,
+            tuesday   : strings.tuesday,
+            wednesday : strings.wednesday,
+            thursday  : strings.thursday,
+            friday    : strings.friday,
+            saturday  : strings.saturday,
+            sunday    : strings.sunday
+        };
+
+        const stringRanges = {
+            morning   : strings.morning,
+            afternoon : strings.afternoon,
+            night     : strings.night,
+        };
+
+        return (
+            <div className="views">
+                <div className="view view-main proposals-experience-availability-view">
+                    <TopNavBar
+                        background={'transparent'}
+                        iconLeft={'arrow-left'}
+                        firstIconRight={'x'}
+                        textCenter={strings.publishProposal}
+                        textSize={'small'}
+                        onLeftLinkClickHandler={this.topNavBarLeftLinkClick}
+                        onRightLinkClickHandler={this.topNavBarRightLinkClick}/>
+                    <div className="proposals-experience-availability-wrapper">
+                        <h2>{strings.title}</h2>
+                        <div className="proposals-experience-availability-frame-wrapper">
+                            <Frame
+                                onClickHandler={this.onClickAvailabilityHandler}>
+                                <div className={'rounded-icon-wrapper'}>
+                                    <RoundedIcon
+                                        icon={'calendar'}
+                                        size={'small'}
+                                        color={'#2B3857'}
+                                        background={'#FFFFFF'}
+                                        border={'1px solid #F0F1FA'}/>
+                                </div>
+                                <div className="text-wrapper">
+                                    <div className="title small">{strings.availabilityTitle}</div>
+                                    {availability ? (
+                                        <div className="resume small">
+                                            {availability.dynamic.map((day, index) =>
+                                                <div key={index}>
+                                                    {dailyWeekdayOptions[day.weekday]}
+                                                    , {strings.scheduleOf}
+                                                    {day.range.map((range, rangeIndex) =>
+                                                        <span key={rangeIndex}> {day.range.length === rangeIndex + 1 ? strings.and + ' ' + stringRanges[range] : stringRanges[range]}</span>
+                                                    )}
+                                                </div>
+                                            )}
+
+                                            {availability.static.map((day, index) =>
+                                                <div key={index}>
+                                                    {strings.from} {day.days.start} {strings.to} {day.days.end}
+                                                    , {strings.scheduleOf}
+                                                    {day.range.map((range, rangeIndex) =>
+                                                        <span key={rangeIndex}> {day.range.length === rangeIndex + 1 ? strings.and + ' ' + stringRanges[range] : stringRanges[range]}</span>
+                                                    )}
+                                                </div>
+                                            )}
+                                        </div>
+                                    ) : (
+                                        <div className="resume small">{strings.availabilityDescription}</div>
+                                    )}
+                                </div>
+                            </Frame>
+                        </div>
+                        <Frame>
+                            <div className={'rounded-icon-wrapper'}>
+                                <RoundedIcon
+                                    icon={'users'}
+                                    size={'small'}
+                                    color={'#2B3857'}
+                                    background={'#FFFFFF'}
+                                    border={'1px solid #F0F1FA'}/>
+                            </div>
+                            <div className="text-participants-wrapper">
+                                <div className="title small">{strings.participantsTitle}</div>
+                            </div>
+                            <div className={'participants-number'}>
+                                <RoundedIcon
+                                    disabled={disableSubstract}
+                                    color={disableSubstract?'#818fa1':'#FFFFFF'}
+                                    background={disableSubstract?'#FFFFFF':'#7bd47e'}
+                                    icon={'minus'} size={'small'}
+                                    border={'1px solid #F0F1FA'}
+                                    onClickHandler={this.onClickParticipantsSubstractHandler}/>
+                                <div className={'participants-number-text'}>{participantLimit}</div>
+                                <RoundedIcon
+                                    background={'#7bd47e'}
+                                    icon={'plus'}
+                                    size={'small'}
+                                    onClickHandler={this.onClickParticipantsPlusHandler}/>
+                            </div>
+                        </Frame>
+                    </div>
+                </div>
+                <StepsBar
+                    color={'green'}
+                    totalSteps={5}
+                    currentStep={3}
+                    continueText={strings.stepsBarContinueText}
+                    cantContinueText={strings.stepsBarCantContinueText}
+                    canContinue={canContinue}
+                    onClickHandler={this.handleStepsBarClick}/>
+            </div>
+        );
+    }
+}
+
+AvailabilityPage.defaultProps = {
+    strings: {
+        publishProposal         : 'Publish proposal',
+        title                   : 'What availability and number of people need?',
+        availabilityTitle       : 'Availability',
+        availabilityDescription : 'Indicate in what time or range of days',
+        participantsTitle       : 'Number of participants',
+        stepsBarContinueText    : 'Continue',
+        stepsBarCantContinueText: 'Indicate at least one parameter',
+        monday                  : 'Monday',
+        tuesday                 : 'Tuesday',
+        wednesday               : 'Wednesday',
+        thursday                : 'Thursday',
+        friday                  : 'Friday',
+        saturday                : 'Saturday',
+        sunday                  : 'Sunday',
+        and                     : 'and',
+        scheduleOf              : 'schedule of',
+        morning                 : 'morning',
+        afternoon               : 'afternoon',
+        night                   : 'night',
+        from                    : 'From',
+        to                      : 'to',
+    }
+};
---
+++ src/js/pages/Proposals/Experience/BasicPage.js
@@ -0,0 +1,169 @@
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import translate from '../../../i18n/Translate';
+import TopNavBar from '../../../components/TopNavBar/TopNavBar.js';
+import '../../../../scss/pages/proposals/experience/basic.scss';
+import StepsBar from "../../../components/ui/StepsBar/StepsBar";
+import Input from "../../../components/ui/Input/Input";
+import Textarea from "../../../components/ui/Textarea/Textarea";
+import CreatingProposalStore from '../../../stores/CreatingProposalStore';
+import * as ProposalActionCreators from "../../../actions/ProposalActionCreators";
+import SelectInline from "../../../components/ui/SelectInline/SelectInline";
+
+@translate('ProposalsExperienceBasicPage')
+export default class BasicPage extends Component {
+
+    static propTypes = {
+        // Injected by @translate:
+        strings     : PropTypes.object,
+        canContinue : PropTypes.bool,
+    };
+
+    static contextTypes = {
+        router : PropTypes.object.isRequired
+    };
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            type        : '',
+            title       : '',
+            description : '',
+        };
+
+        this.topNavBarLeftLinkClick = this.topNavBarLeftLinkClick.bind(this);
+        this.topNavBarRightLinkClick = this.topNavBarRightLinkClick.bind(this);
+        this.handleTitleChange = this.handleTitleChange.bind(this);
+        this.handleDescriptionChange = this.handleDescriptionChange.bind(this);
+        this.handleSelectInlineClick = this.handleSelectInlineClick.bind(this);
+        this.handleStepsBarClick = this.handleStepsBarClick.bind(this);
+    }
+
+    componentWillMount() {
+        if (CreatingProposalStore.proposal.fields) {
+            if (CreatingProposalStore.proposal.type) {
+                this.setState({
+                    type : CreatingProposalStore.proposal.type,
+                });
+            }
+            if (CreatingProposalStore.proposal.fields.title) {
+                this.setState({
+                    title : CreatingProposalStore.proposal.fields.title,
+                });
+            }
+            if (CreatingProposalStore.proposal.fields.description) {
+                this.setState({
+                    description : CreatingProposalStore.proposal.fields.description,
+                });
+            }
+        }
+    }
+
+    topNavBarLeftLinkClick() {
+        this.context.router.push('/proposals-experience-introduction');
+    }
+
+    topNavBarRightLinkClick() {
+        this.context.router.push('/proposals');
+    }
+
+    handleTitleChange(event) {
+        this.setState({title : event});
+    }
+
+    handleDescriptionChange(event) {
+        this.setState({description : event});
+    }
+
+    handleSelectInlineClick(event) {
+        this.setState({type : event});
+    }
+
+    handleStepsBarClick() {
+        const proposal = {
+            type : this.state.type,
+            fields : {
+                title       : this.state.title,
+                description : this.state.description,
+            }
+        };
+        ProposalActionCreators.mergeCreatingProposal(proposal);
+        this.context.router.push('/proposals-experience-type');
+    }
+
+    render() {
+        const {strings} = this.props;
+        const canContinue = this.state.title !== "" && this.state.description !== "" && this.state.type !== "";
+
+        const TypeChoices = [
+            {
+                id: "events",
+                text: strings.events
+            },
+            {
+                id: "gourmet",
+                text: strings.gourmet
+            },
+            {
+                id: "plan",
+                text: strings.plan
+            }
+        ];
+        return (
+            <div className="views">
+                <div className="view view-main proposals-experience-basic-view">
+                    <TopNavBar
+                        background={'transparent'}
+                        iconLeft={'arrow-left'}
+                        firstIconRight={'x'}
+                        textCenter={strings.publishProposal}
+                        textSize={'small'}
+                        onLeftLinkClickHandler={this.topNavBarLeftLinkClick}
+                        onRightLinkClickHandler={this.topNavBarRightLinkClick}/>
+                    <div className="proposals-experience-basic-wrapper">
+                        <h2>{strings.title}</h2>
+                        <SelectInline
+                            onClickHandler={this.handleSelectInlineClick}
+                            color={'green'}
+                            options={TypeChoices}
+                            defaultOption={this.state.type[0]}/>
+                        <div className={'image-wrapper'}>
+                            <img src={'http://via.placeholder.com/480x240'}/>
+                        </div>
+                        <Input
+                            size={'small'}
+                            placeholder={strings.titlePlaceholder}
+                            defaultValue={this.state.title}
+                            onChange={this.handleTitleChange}/>
+                        <Textarea
+                            defaultValue={this.state.description}
+                            onChange={this.handleDescriptionChange}
+                            placeholder={strings.descriptionPlaceholder}/>
+                    </div>
+                </div>
+                <StepsBar
+                    color={'green'}
+                    totalSteps={5}
+                    currentStep={0}
+                    continueText={strings.stepsBarContinueText}
+                    cantContinueText={strings.stepsBarCantContinueText}
+                    canContinue={canContinue}
+                    onClickHandler={this.handleStepsBarClick}/>
+            </div>
+        );
+    }
+}
+
+BasicPage.defaultProps = {
+    strings: {
+        publishProposal          : 'Publish proposal',
+        title                    : 'What is your proposal?',
+        events                   : 'Events',
+        gourmet                  : 'Gourmet',
+        plan                     : 'Plan',
+        titlePlaceholder         : 'Propose title',
+        descriptionPlaceholder   : 'Explain how you want to carry it out...',
+        stepsBarContinueText     : 'Continue',
+        stepsBarCantContinueText : 'You cannot continue',
+    }
+};

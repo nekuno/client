3bd7c87cd0bd772b1594cc10755fd4de5eac7155
yawmoght <yawmoght@gmail.com>
2018-11-30 13:08:47 +0100

    Fix some creating proposal logic

--- src/js/stores/CreatingProposalStore.js
+++ src/js/stores/CreatingProposalStore.js
@@ -1,18 +1,17 @@
 import ActionTypes from '../constants/ActionTypes';
 import BaseStore from './BaseStore';
-import { getValidationErrors } from '../utils/StoreUtils';
 
 class CreatingProposalStore extends BaseStore {
 
     setInitial() {
-        this._proposal = {}
+        this._proposal = {'fields': {}}
     }
 
     _registerToActions(action) {
         super._registerToActions(action);
         switch (action.type) {
             case ActionTypes.MERGE_CREATING_PROPOSAL:
-                Object.assign(this._proposal, action.data);
+                this._mergeData(action.data);
                 this.emitChange();
                 break;
             case ActionTypes.CLEAN_CREATING_PROPOSAL:
@@ -22,6 +21,28 @@ class CreatingProposalStore extends BaseStore {
             default:
                 break;
         }
+    }
+
+    _mergeData(data)
+    {
+        Object.keys(data).forEach(function(key) {
+
+            if (key === 'fields'){
+                Object.keys(data[key]).forEach((fieldKey => {
+                    const fieldValue = data[key][fieldKey];
+                    this._proposal.fields[fieldKey] = fieldValue;
+                }));
+                return;
+            }
+
+            const baseLevelKeys = ['filters', 'type', 'locale', 'availability', 'participantLimit'];
+            if (baseLevelKeys.includes(key)){
+                Object.assign(this._proposal, data);
+            } else {
+                Object.assign(this._proposal.fields, data);
+            }
+
+        }.bind(this));
     }
 
     get proposal() {

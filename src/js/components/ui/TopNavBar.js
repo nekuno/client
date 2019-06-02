6f063136880a25e7662dca62065d8e6898ea9dd4
Elizabeth Martín Campos <elizabeth@nekuno.com>
2019-06-02 18:31:09 +0200

    Fixed navbar

--- src/js/components/ui/TopNavBar.js
+++ src/js/components/ui/TopNavBar.js
@@ -1,45 +1,82 @@
-import PropTypes from 'prop-types';
-import React, { Component } from 'react';
-import shouldPureComponentUpdate from 'react-pure-render/function';
-import TopLeftMenuLink from './TopLeftMenuLink';
-import TopLeftLink from './TopLeftLink';
-import TopRightLink from './TopRightLink';
-import RegularTopTitle from './RegularTopTitle';
+import PropTypes from "prop-types";
+import React, { Component } from "react";
+import shouldPureComponentUpdate from "react-pure-render/function";
+import TopLeftMenuLink from "./TopLeftMenuLink";
+import TopLeftLink from "./TopLeftLink";
+import TopRightLink from "./TopRightLink";
+import RegularTopTitle from "./RegularTopTitle";
 
 export default class TopNavBar extends Component {
 	static propTypes = {
-		leftMenuIcon		   		 : PropTypes.bool,
-		leftIcon			   		 : PropTypes.string,
-		leftText			   		 : PropTypes.string,
-		centerText			   		 : PropTypes.string,
-		centerTextSize	       		 : PropTypes.string,
-		bottomText   	       		 : PropTypes.string,
-		rightIcon              		 : PropTypes.string,
-		secondRightIcon        		 : PropTypes.string,
-		rightText              		 : PropTypes.string,
-		rightIconsWithoutCircle		 : PropTypes.bool,
-		onLeftLinkClickHandler 		 : PropTypes.func,
-		onCenterLinkClickHandler     : PropTypes.func,
-		onRightLinkClickHandler      : PropTypes.func,
+		leftMenuIcon: PropTypes.bool,
+		leftIcon: PropTypes.string,
+		leftText: PropTypes.string,
+		centerText: PropTypes.string,
+		centerTextSize: PropTypes.string,
+		bottomText: PropTypes.string,
+		rightIcon: PropTypes.string,
+		secondRightIcon: PropTypes.string,
+		rightText: PropTypes.string,
+		rightIconsWithoutCircle: PropTypes.bool,
+		onLeftLinkClickHandler: PropTypes.func,
+		onCenterLinkClickHandler: PropTypes.func,
+		onRightLinkClickHandler: PropTypes.func,
 		onSecondRightLinkClickHandler: PropTypes.func,
-		transparentBackground        : PropTypes.bool,
+		transparentBackground: PropTypes.bool
 	};
 
 	shouldComponentUpdate = shouldPureComponentUpdate;
 
 	render() {
-		const {leftMenuIcon, leftIcon, leftText, centerText, centerTextSize, bottomText, rightIcon, secondRightIcon, rightText, rightIconsWithoutCircle, onLeftLinkClickHandler, onCenterLinkClickHandler, onRightLinkClickHandler, onSecondRightLinkClickHandler, transparentBackground} = this.props;
-		const navBarClass = transparentBackground ? 'navbar transparent-background-navbar' : 'navbar';
-		const navBarInnerClass = secondRightIcon ? 'two-right-icons-navbar navbar-inner' : 'navbar-inner';
+		const {
+			leftMenuIcon,
+			leftIcon,
+			leftText,
+			centerText,
+			centerTextSize,
+			bottomText,
+			rightIcon,
+			secondRightIcon,
+			rightText,
+			rightIconsWithoutCircle,
+			onLeftLinkClickHandler,
+			onCenterLinkClickHandler,
+			onRightLinkClickHandler,
+			onSecondRightLinkClickHandler,
+			transparentBackground
+		} = this.props;
+		const navBarClass = transparentBackground
+			? "navbar transparent-background-navbar"
+			: "navbar";
+		const navBarInnerClass = secondRightIcon
+			? "two-right-icons-navbar navbar-inner"
+			: "navbar-inner";
 		return (
 			<div className={navBarClass}>
-				<div id="navbar-inner" className={navBarInnerClass}>
-					<div className="row">
-						{leftMenuIcon ? <TopLeftMenuLink /> : <TopLeftLink icon={leftIcon} wrapIcon={transparentBackground} text={leftText} onClickHandler={onLeftLinkClickHandler}/>}
-						<RegularTopTitle text={centerText} textSize={centerTextSize} bottomText={bottomText} onClickHandler={onCenterLinkClickHandler}/>
-						<TopRightLink icon={rightIcon} secondIcon={secondRightIcon} text={rightText} iconsWithoutCircle={rightIconsWithoutCircle} onClickHandler={onRightLinkClickHandler} onSecondIconClickHandler={onSecondRightLinkClickHandler} />
-					</div>
-				</div>
+				{leftMenuIcon ? (
+					<TopLeftMenuLink />
+				) : (
+					<TopLeftLink
+						icon={leftIcon}
+						wrapIcon={transparentBackground}
+						text={leftText}
+						onClickHandler={onLeftLinkClickHandler}
+					/>
+				)}
+				<RegularTopTitle
+					text={centerText}
+					textSize={centerTextSize}
+					bottomText={bottomText}
+					onClickHandler={onCenterLinkClickHandler}
+				/>
+				<TopRightLink
+					icon={rightIcon}
+					secondIcon={secondRightIcon}
+					text={rightText}
+					iconsWithoutCircle={rightIconsWithoutCircle}
+					onClickHandler={onRightLinkClickHandler}
+					onSecondIconClickHandler={onSecondRightLinkClickHandler}
+				/>
 			</div>
 		);
 	}
